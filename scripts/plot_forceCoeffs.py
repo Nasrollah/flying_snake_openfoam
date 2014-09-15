ccb72f39727ef5dedcf72c53c2b3de11de9582b1
mesnardo <mesnardo@gwu.edu>
2014-09-15 18:43:43 -0400

    Create script to plot aerodynamic coefficients
    See command-line argument `--help` to display possibilities.

--- scripts/plot_forceCoeffs.py
+++ scripts/plot_forceCoeffs.py
@@ -1,98 +1,118 @@
+#!/usr/bin/env python
+
 # file: $OPENFOAM_CASE_DIR/postProcessing/plot_forceCoeffs.py
 # author: Olivier Mesnard (mesnardo@gwu.edu)
 # description: plots aerodynamic coefficients of the flying snake
 
-# Plot aerodynamic coefficients of the flying snake
-# Olivier Mesnard
-# mesnardo@gwu.edu
 
-import matplotlib.pyplot as plt
-import sys
 import os
+import argparse
 
-def getMeanValue(t,val):
-	### To change if needed ###
-	tStart,tEnd = 20.0,80.0
-	### - ###
-	mean = 0.
-	index = 0
-	for i in range(len(t)):
-		if (tStart<=t[i]<=tEnd):
-			mean += val[i]
-			index += 1
-	if (index != 0): mean = mean/index
-	else: mean = None
-	return mean
+import numpy as np
+from matplotlib import pyplot as plt
 
-def main(arg):
 
-	### To change if needed ###
-	Re = 1000
-	AOA = 25
-	meshSize = '450K'
-	### - ###
+def read_inputs():
+	"""Parses the command-line."""
+	# create the parser
+	parser = argparse.ArgumentParser(description='Plots the lift and drag '
+									'coefficients for a given simulation')
+	# fill the parser with arguments
+	parser.add_argument('--case', dest='case', type=str, default='.',
+						help='path of the OpenFOAM case')
+	parser.add_argument('--show', dest='show', action='store_true',
+						help='displays the aerodynamic coefficients')
+	parser.add_argument('--start', dest='start', type=float, default=None,
+						help='starting-time to compute mean coefficients '
+							 ' and to plot instantaneous values')
+	parser.add_argument('--end', dest='end', type=float, default=None,
+						help='ending-time to compute mean coefficients '
+							 ' and to plot instantaneous values')
+	parser.add_argument('--cuibm', dest='cuibm_path', type=str, default=None,
+						help='path of cuIBM force coefficients for comparison')
+	parser.add_argument('--output', '-o', dest='output', type=str, 
+						default='force_coefficients',
+						help='name of the file generated')
+	return parser.parse_args()
 
-	pwd = os.path.dirname(os.path.abspath(__file__))
 
-	fileList = sorted(os.listdir(pwd+'/forceCoeffs'))
-	t,Cm,Cd,Cl = [],[],[],[]
-	for i in range(len(fileList)):
-		inFile = open(pwd+'/forceCoeffs/'+fileList[i]+'/forceCoeffs.dat')
-		for line in inFile:
-			data = line.split()
-			if ('#' in data):
-				pass
-			else:
-				t.append(float(data[0]))
-				Cm.append(float(data[1]))
-				Cd.append(float(data[2]))
-				Cl.append(float(data[3]))
-		inFile.close()
+def main():
+	"""Plots aerodynamic coefficients."""
+	# parse the command-line
+	args = read_inputs()
 
-	Cd_mean = getMeanValue(t,Cd)
-	Cl_mean = getMeanValue(t,Cl)
-	print 'OpenFOAM - mean values: Cd=',Cd_mean,' - Cl=',Cl_mean
-
-	plt.figure(0)
-	plt.grid(True)
-	plt.xlabel(r'time (s)',fontsize=12)
-	plt.ylabel(r'Coefficients',fontsize=12)
-	myLegend = []
-	plt.plot(t,Cd,'b-',linewidth=2)
-	myLegend.append(r'$C_d$ - icoFoam '+meshSize)
-	plt.plot(t,Cl,'g-',linewidth=2)
-	myLegend.append(r'$C_l$ - icoFoam '+meshSize)
-
-	if ('--cuIBM' in arg):
-		t_cuIBM,Cd_cuIBM,Cl_cuIBM = [],[],[]
-		inFile = open(pwd+'/forceCoeffs_Re'+str(Re)+'_AOA'+str(AOA)+'_cuIBM','r')
-		for line in inFile:
-			data = line.split()
-			t_cuIBM.append(float(data[0]))
-			Cd_cuIBM.append(2.*float(data[1]))
-			Cl_cuIBM.append(2.*float(data[2]))
-		inFile.close()
-		plt.plot(t_cuIBM,Cd_cuIBM,'k-.',linewidth=2)
-		myLegend.append(r'$C_d$ - cuIBM')
-		plt.plot(t_cuIBM,Cl_cuIBM,'k--',linewidth=2)
-		myLegend.append(r'$C_l$ - cuIBM')
-		Cd_mean_cuIBM = getMeanValue(t_cuIBM,Cd_cuIBM)
-		Cl_mean_cuIBM = getMeanValue(t_cuIBM,Cl_cuIBM)
-		print 'cuIBM - mean values: Cd=',Cd_mean_cuIBM,' - Cl=',Cl_mean_cuIBM
-	plt.title('--- Flying Snake - Re='+str(Re)+' - AOA='+str(AOA)+' ---')
-	plt.legend(myLegend,'best',prop={'size':12})
-	plt.annotate(r'<$C_l$>='+str(Cl_mean)+'\n<$C_d$>='+str(Cd_mean),xy=(0,0.6),xytext=(0,0.6),\
-				bbox=dict(fc='white'))
-	plt.xlim(t[0],t[-1])
-	plt.ylim(0.5,3.)
-	if ('--save' in arg):
-		plt.savefig(pwd+'/forceCoeffs_'+str(Re)+'_'+str(AOA)+'.png')
-	if ('--show' in arg): plt.show()
+	# initialization
+	t, cd, cl = np.empty(0), np.empty(0), np.empty(0)
 	
-#---------------------------------------
+	# read force coefficients from files
+	force_path = '%s/postProcessing/forceCoeffs/' % args.case
+	folders = sorted(os.listdir(force_path))
+	for folder in folders:
+		with open(force_path+folder+'/forceCoeffs.dat') as infile:
+			t_tmp, cd_tmp, cl_tmp = np.loadtxt(infile, dtype=float, 
+											   usecols=(0,2,3), delimiter='\t',
+											   unpack=True)
+		t = np.append(t, t_tmp)
+		cd = np.append(cd, cd_tmp)
+		cl = np.append(cl, cl_tmp)
 
-#---------------------------------------
-if (__name__ == '__main__'):
-	print '\nplotting aerodynamic coefficients...\n'
-	main(sys.argv)
-#---------------------------------------
+	# calculates boundary indices
+	t_start = (t[0] if not args.start else args.start)
+	t_end = (t[-1] if not args.end else args.end)
+	i_start = np.where(t >= t_start)[0][0]
+	i_end = np.where(t >= t_end)[0][0]-1
+
+	# keep useful slices
+	t = t[i_start:i_end].copy()
+	cd = cd[i_start:i_end].copy()
+	cl = cl[i_start:i_end].copy()
+
+	# compute mean coefficients
+	cd_mean = cd.sum()/cd.size
+	cl_mean = cl.sum()/cl.size
+
+	# output print
+	print '\t--> mean drag coefficient: %g' % cd_mean
+	print '\t--> mean lift coefficient: %g' % cl_mean
+
+	# plot instantaneous force coefficients
+	plt.figure()
+	plt.grid(True)
+	plt.xlabel('Times', fontsize=16)
+	plt.ylabel('Force Coefficients', fontsize=16)
+	plt.plot(t, cd, label=r'$C_d$', color='r', ls='-', lw=2)
+	plt.plot(t, cl, label=r'$C_l$', color='b', ls='-', lw=2)
+	if args.cuibm_path:
+		# read cuIBM force coefficients
+		with open(args.cuibm_path, 'r') as infile:
+			t_cuibm, cd_cuibm, cl_cuibm = np.loadtxt(infile, dtype=float, 
+													 delimiter='\t', 
+													 unpack=True)
+		# keep useful slices
+		i_start = np.where(t_cuibm >= t_start)[0][0]
+		i_end = np.where(t_cuibm >= t_end)[0][0]-1
+		t_cuibm = t_cuibm[i_start:i_end].copy()
+		# multiplied by factor 2.0 because forgot in cuIBM
+		cd_cuibm = 2.*cd_cuibm[i_start:i_end].copy()
+		cl_cuibm = 2.*cl_cuibm[i_start:i_end].copy()
+		
+		# compute mean coefficients
+		cd_cuibm_mean = cd_cuibm.sum()/cd_cuibm.size
+		cl_cuibm_mean = cl_cuibm.sum()/cl_cuibm.size
+
+		# output print
+		print '\t--> mean drag coefficient with cuIBM: %g' % cd_cuibm_mean
+		print '\t--> mean lift coefficient with cuIBM: %g' % cl_cuibm_mean
+
+		# add cuIBM instantaneous values to the figure
+		plt.plot(t_cuibm, cd_cuibm, label=r'$C_d$ - cuIBM', color='r', ls='--')
+		plt.plot(t_cuibm, cl_cuibm, label=r'$C_l$ - cuIBM', color='b', ls='--')
+
+	plt.legend(loc='best', prop={'size': 16})
+	plt.savefig('%s/postProcessing/%s.png' % (args.case, args.output))
+	if args.show:
+		plt.show()
+
+
+if __name__ == '__main__':
+	main()
