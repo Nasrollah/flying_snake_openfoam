3a53829b29b8fae7211384d25a9db50bd3939253
mesnardo <mesnardo@gwu.edu>
2014-09-16 18:32:20 -0400

    Change mpirun path
    Because mpirun run was a link to anaconda,
    I created a variable in the bash script that points
    to the mpirun of Third-Party-2.2.2.

--- scripts/run_case.sh
+++ scripts/run_case.sh
@@ -14,7 +14,9 @@ LOG_PATH=$CASE_PATH/simu.log
 decomposePar -case $CASE_PATH > $LOG_PATH
 
 # run the simulation in parallel
-mpirun -np 6 icoFoam -case $CASE_PATH -parallel >> $LOG_PATH
+
+MPI_RUN=$WM_THIRD_PARTY_DIR/platforms/linux64Gcc/openmpi-1.6.3/bin/mpirun
+$MPI_RUN -np 6 icoFoam -case $CASE_PATH -parallel >> $LOG_PATH
 
 # reconstruct the solution
 reconstructPar -case $CASE_PATH >> $LOG_PATH
