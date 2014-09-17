aad63a58bf85e046f9f7fc76905fe690f3c19f61
mesnardo <mesnardo@gwu.edu>
2014-09-17 14:31:10 -0400

    Remove useless optional arguments

--- scripts/plot_vorticity.sh
+++ scripts/plot_vorticity.sh
@@ -11,6 +11,5 @@ FLYING_SNAKE_DIR="/home/mesnardo/flying_snake_openfoam"
 echo "\nPlot and Save the vorticity field around the flying snake"
 echo "\t--> OpenFOAM case: $CASE_PATH\n"
 
-paraFoam -case $CASE_PATH -touch >> $CASE_PATH/log
-pvbatch --use-offscreen-rendering $FLYING_SNAKE_DIR/scripts/plot_vorticity.py \
-		--case $CASE_PATH >> $CASE_PATH/log
+pvbatch $FLYING_SNAKE_DIR/scripts/plot_vorticity.py \
+		--case $CASE_PATH
