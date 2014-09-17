965b79db683c52ab4e3e052892282286fcb3f5c8
mesnardo <mesnardo@gwu.edu>
2014-09-17 09:11:13 -0400

    Add variable OPENFOAM_MPIRUN to bashrc
    Update the bash script to run a case in parallel.

--- scripts/run_case.sh
+++ scripts/run_case.sh
@@ -14,9 +14,7 @@ LOG_PATH=$CASE_PATH/simu.log
 decomposePar -case $CASE_PATH > $LOG_PATH
 
 # run the simulation in parallel
-
-MPI_RUN=$WM_THIRD_PARTY_DIR/platforms/linux64Gcc/openmpi-1.6.3/bin/mpirun
-$MPI_RUN -np 6 icoFoam -case $CASE_PATH -parallel >> $LOG_PATH
+$OPENFOAM_MPIRUN -np 6 icoFoam -case $CASE_PATH -parallel >> $LOG_PATH
 
 # reconstruct the solution
 reconstructPar -case $CASE_PATH >> $LOG_PATH
