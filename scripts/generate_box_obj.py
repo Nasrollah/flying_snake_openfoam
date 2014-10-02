0690d8110caa4ad3ee969031c9e4d3059a2ad6eb
mesnardo <mesnardo@gwu.edu>
2014-10-02 18:13:39 -0400

    Generate a 2D triangulated box
    The OBJ file generated should be used to mesh just around it
    with snappyHexMesh.
    It should accelerate the process of the 3D meshing tool
    when onyl the front patch is needed (i.e. for a 2D simulation).

--- scripts/generate_box_obj.py
+++ scripts/generate_box_obj.py
@@ -11,114 +11,111 @@ import numpy
 
 
 class Vertex:
+	"""Contains info about a vertex."""
 	def __init__(self, x ,y, index):
+		"""Initializes a vertex by its coordinates and its index in the mesh.
+		
+		Arguments
+		---------
+		x, y -- coordinates of the vertex.
+		index -- index of the vertex in the mesh.
+		"""
 		self.x, self.y = x, y
 		self.index = index
 
 
 class Face:
+	"""Contains info about a face."""
 	def __init__(self, vertex_1, vertex_2, vertex_3):
+		"""Initializes a face by its vertices.
+		
+		Arguments
+		---------
+		vertex_1, vertex_2, vertex_3 -- Vertex objects that compose the face.
+		"""
 		self.vertex_1 = vertex_1
 		self.vertex_2 = vertex_2
 		self.vertex_3 = vertex_3
 
 
+def read_inputs():
+	"""Parses the command-line."""
+	# create the parser
+	parser = argparse.ArgumentParser(description='Generates a 2D triangulated '
+												 'box and writes into a '
+												 'Wavefront OBJ file')
+	# fill the parser with arguments
+	parser.add_argument('--bottom-left', '-bl', dest='bl', type=float, 
+						nargs='+', default=[-2.0, -2.0],
+						help='coordinates of the bottom-left corner of the box')
+	parser.add_argument('--top-right', '-tr', dest='tr', type=float, 
+						nargs='+', default=[2.0, 2.0],
+						help='coordinates of the top-right corner of the box')
+	parser.add_argument('-z', dest='z', type=float, default=1.0,
+						help='z-coordinate of the box')
+	parser.add_argument('-n', dest='n', type=int, nargs='+', default=[100, 100],
+						help='number of points in the x- and y- directions')
+	parser.add_argument('--output', dest='output_name', type=str, 
+						default='box',
+						help='name of the OBJ file without the extension')
+	return parser.parse_args()
+
+
 def main():
+	"""Generates a 2D triangulated box and writes in Wavefront OBJ file."""
+	# parse the command-line
+	args = read_inputs()
+
 	# coorfinates of the 2D box
-	x_bl, y_bl = 0.0, 0.0
-	x_tr, y_tr = 3.0, 3.0
+	x_bl, y_bl = args.bl[0], args.bl[1]
+	x_tr, y_tr = args.tr[0], args.tr[1]
 
 	# number of points in each directions
-	nx, ny = 4, 4
+	nx, ny = args.n[0], args.n[1]
 
+	# create x-stations and y-stations
 	x = numpy.linspace(x_bl, x_tr, nx)
 	y = numpy.linspace(y_bl, y_tr, ny)
 
+	# store the vertices
 	vertices = numpy.empty(nx*ny, dtype=object)
 	for j in xrange(ny):
 		for i in xrange(nx):
 			vertices[j*nx+i] = Vertex(x[i], y[j], j*nx+i)
-	
-	horizontal_edges = numpy.empty((nx-1)*ny, dtype=object)
-	vertical_edges = numpy.empty(nx*(ny-1), dtype=object)
-	diagonal_edges = numpy.empty((nx-1)*(ny-1), dtype=object)
-
-	for j in xrange(ny):
-		for i in xrange(nx-1):
-			horizontal_edges[j*(nx-1)+i] = Edge(vertices[j*nx+i], 
-												vertices[j*nx+i+1])
-
-	for j in xrange(ny-1):
-		for i in xrange(nx):
-			vertical_edges[j*nx+i] = Edge(vertices[j*nx+i], 
-										  vertices[(j+1)*nx+i])
-
-	for j in xrange(ny-1):
-		for i in xrange(nx-1):
-			diagonal_edges[j*(nx-1)+i] = Edge(vertices[j*nx+i+1],
-											  vertices[(j+1)*nx+i])
 
 
-	for i, edge in enumerate(diagonal_edges):
-		print '%d - (%g,%g) - (%g,%g)' % (i, 
-										  edge.start.x, edge.start.y,
-										  edge.end.x, edge.end.y)
-
-	faces = numpy.empty(2*(nx-1)*(ny-1), dtype=object) 
-
+	# create the faces (lower and upper triangles)
 	lower_faces = numpy.empty((nx-1)*(ny-1), dtype=object)
 	upper_faces = numpy.empty((nx-1)*(ny-1), dtype=object)
-
 	for j in xrange(ny-1):
 		for i in xrange(nx-1):
-			lower_faces[j*(nx-1)+i] = Face(horizontal_edges[j*(nx-1)+i],
-										   vertical_edges[j*nx+i],
-										   diagonal_edges[j*(nx-1)+i])
-			upper_faces[j*(nx-1)+i] = Face(horizontal_edges[(j+1)*(nx-1)+i],
-										   vertical_edges[j*nx+i+1],
-										   diagonal_edges[j*(nx-1)+i])
-		
-
+			lower_faces[j*(nx-1)+i] = Face(vertices[j*nx+i],
+										   vertices[j*nx+i+1],
+										   vertices[(j+1)*nx+i])
+			upper_faces[j*(nx-1)+i] = Face(vertices[(j+1)*nx+i+1],
+										   vertices[(j+1)*nx+i],
+										   vertices[j*nx+i+1])
 	faces = numpy.insert(upper_faces, 
 						 numpy.arange(len(lower_faces)), 
 						 lower_faces)
 
-
-	for i, face in enumerate(upper_faces):
-		print 'edge1'
-		print '%d - (%g,%g) - (%g,%g)' % (i,
-										  face.edge_1.start.x,
-										  face.edge_1.start.y,
-										  face.edge_1.end.x,
-										  face.edge_1.end.y)
-		print 'edge2'
-		print '%d - (%g,%g) - (%g,%g)' % (i,
-										  face.edge_2.start.x,
-										  face.edge_2.start.y,
-										  face.edge_2.end.x,
-										  face.edge_2.end.y)
-		print 'edge3'
-		print '%d - (%g,%g) - (%g,%g)' % (i,
-										  face.edge_3.start.x,
-										  face.edge_3.start.y,
-										  face.edge_3.end.x,
-										  face.edge_3.end.y)
-
-		header = ( '# Wavefront OBJ file\n'
-				   '# points: %d\n'
-				   '# faces: %d\n'
-				   '# zones: 1\n'
-				   '# regions: 0 %s\n'
-				   % (nx*ny, 2*(nx-1)*(ny-1), 'test'))
-
-		with open('test.obj', 'r') as outfile:
-			outfile.write(header)
-			for vertex in vertices:
-				outfile.write('v %.6f %.6f %.6f\n' % (vertex.x, vertex.y, 1.0))
-			outfile.write('g %s\n' % 'test')
-			for face in faces:
-				outfile.write('f %d %d %d\n' % (face.edge_1.start))
-
+	# write the triangulated box into an OBJ file
+	header = ( '# Wavefront OBJ file\n'
+			   '# points: %d\n'
+			   '# faces: %d\n'
+			   '# zones: 1\n'
+			   '# regions: 0 %s\n'
+			   % (nx*ny, 2*(nx-1)*(ny-1), args.output_name))
+	with open('%s.obj' % args.output_name, 'w') as outfile:
+		outfile.write(header)
+		for vertex in vertices:
+			outfile.write('v %.6f %.6f %.6f\n' % (vertex.x, vertex.y, args.z))
+		outfile.write('g %s\n' % args.output_name)
+		for face in faces:
+			outfile.write('f %d %d %d\n' % (face.vertex_1.index+1,
+											face.vertex_2.index+1,
+											face.vertex_3.index+1))
+	
 
 if __name__ == '__main__':
 	main()
